3c8a6f8f867b3cc5dbb9057ba86a061b71635714
Никита Шевченко <62301874+BornBeBat@users.noreply.github.com>
2023-11-14 21:46:39 +0200

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,7 @@
+const categoris = document.querySelector("#categories");
+console.log("Number of categories:", categoris.children.length); // first answer
+const itemList = document.querySelectorAll("#categories ul");
+itemList.forEach((item) => {
+  console.log("Category:", item.previousElementSibling.textContent);
+  console.log("Elements:", item.children.length);
+});

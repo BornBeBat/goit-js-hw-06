073987a39a59e72d4507e1aadafb453f188ffde1
Никита Шевченко <62301874+BornBeBat@users.noreply.github.com>
2023-11-16 15:01:00 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,8 @@
 const input = document.querySelector("#validation-input");
+const leng = +input.dataset.length;
 const classInp = input.classList;
 input.addEventListener("blur", (event) => {
-  if (event.currentTarget.value.length === 6) {
+  if (event.currentTarget.value.length === leng) {
     classInp.add("valid");
     classInp.remove("invalid");
   } else if (event.currentTarget.value.length === 0) {

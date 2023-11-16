4984f38c23316f1b20d4f0d93b91206eef7c731d
Никита Шевченко <62301874+BornBeBat@users.noreply.github.com>
2023-11-16 16:53:17 +0200

    task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector(".login-form");
+const { email, password } = form.elements;
+form.addEventListener("submit", onFormSubmit);
 
+function onFormSubmit(event) {
+  event.preventDefault();
+  if (email.value === "" || password.value === "") {
+    alert("Enter yours data");
+  } else {
+    const nevArr = {
+      email: email.value,
+      password: password.value,
+    };
+    console.log(nevArr);
+    form.reset();
+  }
+}

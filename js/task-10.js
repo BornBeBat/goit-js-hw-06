89d319b9b8a4f02b9d4396f8431a846fac66ff03
Никита Шевченко <62301874+BornBeBat@users.noreply.github.com>
2023-11-16 18:59:49 +0200

    task-10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,38 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const box = document.querySelector("#boxes");
+const btnCreate = document.querySelector(`button[data-create]`);
+const btnDelete = document.querySelector(`button[data-destroy]`);
+const input = document.querySelector("input");
+btnCreate.addEventListener("click", () => {
+  removeDiv();
+  box.append(...createDivPack(input.value));
+  input.value = 0;
+});
+btnDelete.addEventListener("click", removeDiv);
+
+function createDiv(a) {
+  const el = document.createElement("div");
+
+  el.style.width = `${30 + a * 10}px`;
+  el.style.height = `${30 + a * 10}px`;
+  el.style.backgroundColor = getRandomHexColor();
+  return el;
+}
+
+function createDivPack(number) {
+  const newArr = [];
+  for (let i = 0; i < number; i++) {
+    newArr.push(createDiv(i));
+  }
+  return newArr;
+}
+
+function removeDiv() {
+  const elements = box.children;
+  for (let i = elements.length - 1; i >= 0; i--) {
+    elements[i].remove();
+  }
+}

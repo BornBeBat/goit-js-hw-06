59cf2a3dde9b9aeebb682e4e570e9ad4d056a88e
Никита Шевченко <62301874+BornBeBat@users.noreply.github.com>
2023-11-16 17:22:47 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const colorText = document.querySelector(".color");
+const bodyStyle = document.querySelector("body").style;
+const btn = document.querySelector(".change-color");
+
+btn.addEventListener("click", clickBtn);
+
+function clickBtn(event) {
+  bodyStyle.backgroundColor = getRandomHexColor();
+  colorText.textContent = `${bodyStyle.backgroundColor}`;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)

afee356e9a14f86996634ca87aab607c4749ed8b
Никита Шевченко <62301874+BornBeBat@users.noreply.github.com>
2023-11-16 14:15:46 +0200

    Task-5

---
+++ js/task-05.js
@@ -0,0 +1,6 @@
+const input = document.querySelector("#name-input");
+const text = document.querySelector("#name-output");
+input.addEventListener(
+  "input",
+  (event) => (text.textContent = event.currentTarget.value)
+);
